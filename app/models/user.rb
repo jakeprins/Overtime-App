bedce4196d5208c7a38ae11568d0b16cac5ad0d2
Jake Prins <jakeprins@outlook.com>
2017-03-30 14:58:44 +0200

    Integrated phone validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :posts
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,7 +7,13 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name, :phone
 
+  PHONE_REGEX = /\A[0-9]*\Z/
+
+  validates_format_of :phone, with: PHONE_REGEX
+
+  validates :phone, length: { is: 10 }
+
   def full_name
-    last_name.upcase + ', ' + first_name.upcase
+    last_name.upcase + ", " + first_name.upcase
   end
 end
